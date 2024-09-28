10f130ccff7473983eeef14384ffe6ca9a97eae1
sixtycardpickup <kinsellaaiden@gmail.com>
2024-09-28 00:12:16 -0500

    Update opponents.h

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -864,12 +864,13 @@
 #define TRAINER_GRENDEL_MOTHER              858
 #define TRAINER_BRIEN                       859
 #define TRAINER_SQUORT                      860
+#define TRAINER_ANA_FOG_1                   861
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      861
+#define TRAINERS_COUNT                      862
 #define MAX_TRAINERS_COUNT                  864
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
